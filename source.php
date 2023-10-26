fd0ed8063447f4f204f2ca3d5ffa2375817c9043
Batuhan Karakaya <91139544+Batuhank8931@users.noreply.github.com>
2023-10-26 19:39:05 +0300

    source.php

--- source.php
+++ source.php
@@ -1,18 +1,39 @@
 
 <?php
 
-$conn = new mysqli("127.0.0.1","u161087782_database", "Batu1010!", "u161087782_Batuhandata", "3306");
+/*
+If you want to get the data from your database, please fill the requested data below.
 
-$sql = "SELECT * FROM source_data";
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+$dbport = "your_database_port";
+$tablename = "your_table_name";
+$columnname = "your_column_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+
+$sql = "SELECT * FROM $tablename";
 $result = $conn->query($sql);
 
 
 while ($row = $result->fetch_assoc()) {
-    $jsonString= $row["data"];
+    $jsonString= $row[$columnname];
 }
 
 $data = json_decode($jsonString, true);
 
 $conn->close();
+
+If you activate this section please get comment below section:
+
+*/
+$file_path = 'maindata.txt';
+
+$json = file_get_contents($file_path);
+
+$data = json_decode($json, true);
+
 
 ?>
